397407f29b0920626c30ee1becc980b796d43a43
hugobullont <hugobullont@gmail.com>
2016-10-13 22:52:42 -0500

    Added Stefano's Updates

---
+++ BusinessLogic/CustomerService/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace BusinessLogic.CustomerService
+{
+    public interface ICustomerService
+    {
+        List<Customer> GetAllCustomers();
+
+        void InsertCustomer(Customer objcustomer);
+    }
+}
---
+++ DataAccess/CustomerRepository/CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DataAccess.CustomerRepository
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public List<Entities.Customer> GetAllCustomers()
+        {
+            UPCSecurityEntities UPCModel = new UPCSecurityEntities();
+            List<Customer> customers = (from c in UPCModel.Customers
+                                    select c).ToList();
+            return customers;
+        }
+
+
+        public void InsertCustomer(Customer objcustomer)
+        {
+            using (var model = new UPCSecurityEntities())
+            {
+                model.Customers.Add(objcustomer);
+                model.SaveChanges();
+            }
+        }
+    }
+}
